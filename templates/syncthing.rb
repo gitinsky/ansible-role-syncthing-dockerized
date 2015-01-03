fe9555b4854032f7619a1498a1d8f996261e5470
Alex Chistyakov <alexclear@gmail.com>
2015-01-03 22:23:44 +0300

    First commit

---
+++ templates/syncthing.rb
@@ -0,0 +1,17 @@
+Eye.application 'syncthing' do
+  working_dir '/config'
+  stdall '/var/log/eye/syncthing-stdall.log' # stdout,err logs for processes by default
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
+  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+
+  process :syncthing do
+    pid_file '/var/run/syncthing-eye.pid'
+    start_command '/sbin/setuser syncthing /syncthing -home /config'
+
+    daemonize true
+    start_timeout 10.seconds
+    stop_timeout 5.seconds
+
+  end
+
+end
